cb8cdf71b1062835cd730cd431284f4c71f2379b
kofzx <2670476383@qq.com>
2018-06-11 13:51:07 +0800

    color bug

--- js/canvas.js
+++ js/canvas.js
@@ -1,54 +1,105 @@
-;(function($, window, document, undefined){
+;
+(function($, window, document, undefined) {
 
     var pluginName = "canvas";
     var canvas, ctx, width, height, canvasCssText;
-    var star;
-    function Plugin(element) {
+    var stars = [];
+    var num = 5;
+
+    var lastTime;
+    var deltaTime;
+
+    var starPic = new Image();
+
+    function Plugin(element, options) {
         this.element = element;
+        this.options = $.extend({
+            colors: ["red", "green", "blue"]
+        }, options);
         this.init();
     }
     Plugin.prototype = {
         init: function() {
             $(this.element).css("position", "relative");
-            canvas  = document.createElement("canvas");
-            ctx     = canvas.getContext('2d');
-            width   = $(this.element).outerWidth();
-            height  = $(this.element).outerHeight();
+            canvas = document.createElement("canvas");
+            ctx = canvas.getContext('2d');
+            width = $(this.element).outerWidth();
+            height = $(this.element).outerHeight();
             canvasCssText = "position: absolute; top: 0; left: 0;";
-            canvas.width   = width;
-            canvas.height  = height;
+            canvas.width = width;
+            canvas.height = height;
             canvas.style.cssText = canvasCssText;
             $(this.element).append(canvas);
 
-            star = new Star();
-            star.init();
+            starPic.src = "images/star.png";
+
+            for (var i = 0; i < num; i++) {
+                var obj = new Star(this.options);
+                stars.push(obj);
+                stars[i].init();
+            }
+
+            lastTime = Date.now();
 
             loop();
 
         }
     };
-    $.fn[pluginName] = function() {
-        new Plugin(this);
+    $.fn[pluginName] = function(options) {
+        new Plugin(this, options);
     };
 
-    function Star() {
+    function Star(options) {
         this.x;
         this.y;
+        this.xSpd;
+        this.ySpd;
+        this.scale;
+        this.color;
+        this.options = options;
     }
     Star.prototype.init = function() {
-        this.x = 5;
-        this.y = 10;
+        var colors = this.options.colors;
+        this.scale = Math.random() * 15 + 15;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+        this.x = Math.random() * (width - this.scale * 2) + this.scale;
+        this.y = Math.random() * (height - this.scale * 2) + this.scale;
+        this.xSpd = Math.random() * 1 - 0.5;
+        this.ySpd = Math.random() * 1 - 0.5;
+    };
+    Star.prototype.update = function() {
+        this.x += this.xSpd;
+        this.y += this.ySpd;
     };
     Star.prototype.draw = function() {
-        // 绘制星星、星星偏移、星星消失
-        ctx.moveTo(0,0);
-        ctx.lineTo(this.x,this.y);
-        ctx.lineTo(15,10);
-        ctx.stroke();
+        ctx.save();
+        ctx.drawImage(starPic, this.x, this.y, this.scale, this.scale);
+        // 添加颜色
+        ctx.globalCompositeOperation = "source-atop";
+        ctx.globalAlpha = 0.6;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.scale, this.scale);
+        ctx.restore();
     };
 
+    function clearCanvas() {
+        ctx.clearRect(0, 0, width, height);
+    }
+
+    function drawStars() {
+        for (var i = 0; i < num; i++) {
+            stars[i].update();
+            stars[i].draw();
+        }
+    }
+
     function loop() {
-        star.draw();
+        var now = Date.now();
+        deltaTime = now - lastTime;
+        lastTime = now;
+
+        clearCanvas();
+        drawStars();
         window.requestAnimationFrame(loop);
     }
 
